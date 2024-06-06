6c82d60831587836723dba7e47a4b695ba976a77
Hanbin Cho <mraerok@gmail.com>
2024-06-06 18:08:08 -0400

    Add GraphQL Query for customer

--- src/datasources/analytics.ts
+++ src/datasources/analytics.ts
@@ -5,6 +5,7 @@ export default class AnalyticsDataSource {
   private isConnected: boolean = false;
   private db: Db;
   static readonly dbName = "sample_analytics";
+  static readonly collCust = "customers";
   static readonly collTxns = "transactions";
   static readonly fieldTxns = "transactions";
 
@@ -34,6 +35,20 @@ export default class AnalyticsDataSource {
     return this.db.collection(collectionName);
   }
 
+  async customer(username: string) {
+    const coll = this.getCollection(AnalyticsDataSource.collCust);
+    // TODO: In rare cases, a given username or email
+    //       can resolve to multiple customers:
+    //       - email: 'jenn...9@...' => name: ['Alex ...', 'David ...']
+    //       - username: 'ihi...' => name: ['... Thomas', '... Smith']
+    //       - username: 'pat...5' => name: ['Jamie ...', 'Curtis ...']
+    // For now, just return the first match, but as soon as possible,
+    // understand whether these duplicates are allowed, and if so, in which case,
+    // then handle fetching a unique customer properly.
+    // (Perhaps by a combination of username and email, for example).
+    return await coll.findOne({ username: username });
+  }
+
   // TODO idea: sort the transactions by date at the DB level,
   //            so that the frontend won't have to before displaying to the user.
   //            Something like this could work:
@@ -57,7 +72,13 @@ export default class AnalyticsDataSource {
   // ])
   async transactionBatch(accountId: number) {
     const coll = this.getCollection(AnalyticsDataSource.collTxns);
-    const txnBatch = await coll.findOne({ account_id: accountId });
-    return txnBatch;
+    // TODO: In rare cases, a given account_id
+    //       can resolve to multiple transaction batches:
+    //       - account_id: '627...' returns 2 batches.
+    // For now, just return the first match, but as soon as possible,
+    // understand whether these duplicates are allowed, and if so, in which case,
+    // then handle multiple transaction batches properly.
+    // (Perhaps by merging the batches directly, if there won't be duplicate transactions).
+    return await coll.findOne({ account_id: accountId });
   }
 }
